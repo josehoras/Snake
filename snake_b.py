18c69279a6ed7b099126b1a8f9a7ae76d9ef914d
josehoras <jose.horas@gmail.com>
2020-05-03 22:25:43 +0200

    Code cleanup

--- snake_b.py
+++ snake_b.py
@@ -3,31 +3,32 @@ import numpy as np
 import pygame
 from pygame.locals import *
 
+
 def check_quit_event():
     for event in pygame.event.get():
-        if event.type == KEYDOWN and event.key == K_ESCAPE:
-            return True
-        elif event.type == QUIT:
-            return True
+      if event.type == KEYDOWN and event.key == K_ESCAPE:
+        return True
+      elif event.type == QUIT:
+        return True
     return False
 
 
 def check_continue_event():
     for event in pygame.event.get():
-        if event.type == KEYDOWN and event.key == K_SPACE:
-            return True
+      if event.type == KEYDOWN and event.key == K_SPACE:
+        return True
     return False
 
 
 def check_pause_event():
     for event in pygame.event.get():
-        if event.type == KEYDOWN and event.key == K_SPACE:
-            return True
+      if event.type == KEYDOWN and event.key == K_SPACE:
+        return True
     return False
 
 
 class SnakePart(pygame.sprite.Sprite):
-    def __init__(self, grid, dir, style, sq_size):
+    def __init__(self, grid, dir, style):
         super(SnakePart, self).__init__()
         self.dir = dir
         self.body = pygame.Surface(sq_size)
@@ -50,25 +51,34 @@ class SnakePart(pygame.sprite.Sprite):
         self.rect.topleft = self.pos
 
 
-class Snake(pygame.sprite.Group):
-    def __init__(self, style, screen_size, grid_size):
-        super(Snake, self).__init__()
-        self.grid = grid_size//2 - [[2,0], [1,0], [0,0]]
-        self.direction = np.array([[1, 0], [1, 0], [1, 0]])
+class SnakeFull(pygame.sprite.Group):
+    def __init__(self, style):
+        super(SnakeFull, self).__init__()
+        self.body_grid = np.array(
+            [[grid_x // 2 - 2, grid_y // 2],
+             [grid_x // 2 - 1, grid_y // 2],
+             [grid_x // 2, grid_y // 2]])
+        self.direction = np.array([[1, 0], [1, 0], [1, 0], [1, 0]])
 
-        self.screen_size = screen_size
-        self.sq_size = screen_size // grid_size
         self.style = style
         self.next_dir = self.direction[-1]
         self.length = 3
         self.length_increase = 10
-        self.speed = 0.05
+        self.speed = 0.06
         self.state = "alive"
 
-        self.tail = SnakePart(self.grid[0], self.direction[0], self.style, self.sq_size)
-        for i in range(1, len(self.grid)):
-            self.add(SnakePart(self.grid[i], self.direction[i], self.style, self.sq_size))
-        self.head = SnakePart(self.grid[-1], self.direction[-1], self.style, self.sq_size)
+        self.head = SnakePart(self.body_grid[-1], self.direction[-1], self.style)
+        self.tail = SnakePart(self.body_grid[0], self.direction[0], self.style)
+        for i in range(len(self.body_grid) - 1):
+            self.add(SnakePart(self.body_grid[i+1], self.direction[i+1], self.style))
+
+        self.grid = self.update_grid()
+
+    def update_grid(self):
+        grid = [sp.grid for sp in self]
+        grid.append(self.tail.grid)
+        grid.append(self.head.grid)
+        return grid
 
     def update_move(self, pressed_keys, number_pos):
         self.state = "alive"
@@ -79,28 +89,27 @@ class Snake(pygame.sprite.Group):
         if len(self) >= self.length:
             self.tail.update(self.speed)
         # Check if head has reached a new grid square and add a new part to body
-        if (self.head.rect.topleft % self.sq_size == [0, 0]).all() and \
-                (self.head.rect.topleft // self.sq_size != self.head.grid).any():
-            self.head.grid = self.head.rect.topleft // self.sq_size
+        if (self.head.rect.topleft % sq_size == [0, 0]).all() and \
+                (self.head.rect.topleft // sq_size != self.head.grid).any():
+            self.head.grid = self.head.rect.topleft // sq_size
             self.head.dir = self.next_dir
-            self.add(SnakePart(self.head.grid, self.head.dir, self.style, self.sq_size))
-            self.grid = np.append(self.grid, [self.head.grid], axis=0)
+            self.add(SnakePart(self.head.grid , self.head.dir, self.style))
+            self.grid = self.update_grid()
             if (self.head.grid == number_pos).all():
                 self.length += self.length_increase
                 self.state = "just_ate"
         # Check if tail has reached a new grid square and remove that part of the body
-        if (self.tail.rect.topleft % self.sq_size == [0, 0]).all() and \
-                (self.tail.rect.topleft // self.sq_size != self.tail.grid).any():
-            to_del = [sp for sp in self if (sp.grid == self.tail.rect.topleft // self.sq_size).all()]
-            self.grid = self.grid[[(g != self.tail.grid).any() for g in self.grid]]
-            # print([np.array2string(i, separator=',') for i in self.grid])
+        if (self.tail.rect.topleft % sq_size == [0, 0]).all() and \
+                (self.tail.rect.topleft // sq_size != self.tail.grid).any():
+            to_del = [sp for sp in self if (sp.grid == self.tail.rect.topleft // sq_size).all()]
             self.tail.dir = to_del[0].dir
             self.tail.grid = to_del[0].grid
             self.remove(to_del[0])
+            self.grid = self.update_grid()
         # Check dying conditions
-        if self.head.rect.top < 0 or self.head.rect.bottom > self.screen_size[1]:
+        if self.head.rect.top < 0 or self.head.rect.bottom >= SCREEN_Y:
             self.state = "dead"
-        if self.head.rect.left < 0 or self.head.rect.right > self.screen_size[0]:
+        if self.head.rect.left < 0 or self.head.rect.right >= SCREEN_X:
             self.state = "dead"
         if len(pygame.sprite.spritecollide(self.head, self, False)) > 1:
             self.state = "dead"
@@ -115,23 +124,21 @@ class Snake(pygame.sprite.Group):
         if pressed_keys[K_DOWN] and self.head.dir[1]!=-1:
             self.next_dir = np.array([0, 1])
 
-    def plot(self, screen):
+    def plot(self):
         screen.blit(self.head.body, self.head.rect)
         screen.blit(self.tail.body, self.tail.pos)
         for sp in self:
             screen.blit(sp.body, sp.pos)
 
-
 # Functions to generte numbers and plot messages
 def generate_number(n, thing):
-    n += 1
+    number = n + 1
     c = [0]
-    n_grid = []
-    while c:
-        n_grid = np.array([random.randint(0, grid_size[0]-1), random.randint(0, grid_size[1]-1)])
-        c = [g for g in thing.grid if (n_grid == g).all()]
-    n_txt = font.render(str(n), True, white)
-    return n, n_grid, n_txt
+    while c!=[]:
+        number_grid = np.array([random.randint(0, grid_x), random.randint(0, grid_y)])
+        c = [g for g in thing.grid if (number_grid == g).all()]
+    number_txt = font.render(str(number), True, white)
+    return number, number_grid, number_txt
 
 
 def plot_msg(msg):
@@ -142,26 +149,27 @@ def plot_msg(msg):
 
 
 #MAIN FUNCTION
+SCREEN_X = 400
+SCREEN_Y = 400
+grid_x = 20
+grid_y = 20
+sq_size = np.array([SCREEN_X // (grid_x+1), SCREEN_Y // (grid_y+1)])
 black = 0, 0, 0
 white = 255, 255, 255
 blue = 0, 0, 255
-screen_size = np.array([400, 400])
-grid_size = np.array([20, 20])
-sq_size = screen_size // grid_size
-
 # Start pygame screen
 pygame.init()
-screen = pygame.display.set_mode(screen_size)
+screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 pygame.display.set_caption('Snake')
 # Define fonts for screen messages
 font_size = int(sq_size[1]*1.5)
 font = pygame.font.SysFont("ubuntumono",  font_size)
 
 # Create snake and first number
-snake = Snake("square", screen_size, grid_size)  # style "square" or "round"
+snake = SnakeFull("square")  # style "square" or "round"
 number, number_grid, number_txt = generate_number(0, snake)
 screen.blit(number_txt, number_grid * sq_size - [0, font_size/5])
-snake.plot(screen)
+snake.plot()
 plot_msg("Press Space to start")
 pygame.display.update()
 while not(check_continue_event()):
@@ -172,7 +180,7 @@ while not(check_quit_event()):
     snake.update_move(pygame.key.get_pressed(), number_grid)
     screen.fill(black)
     screen.blit(number_txt, number_grid * sq_size - [font_size / 5 * (number > 9), font_size / 5])
-    snake.plot(screen)
+    snake.plot()
     pygame.display.update()
     if snake.state == "just_ate":
         number, number_grid, number_txt = generate_number(number, snake)
